df8f604c31e998cbacf166e6cfd26bb91d1a7bd8
Murat Gür <murat50122@gmail.com>
2026-05-16 13:34:33 +0300

    auditlogs are completed

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -4,6 +4,7 @@ const Categories = require("../db/models/Categories");
 const Response = require("../lib/Response");
 const CustomError = require("../lib/Error");
 const Enum = require('../config/Enum');
+const AuditLogs = require("../lib/AuditLogs");
 
 
 router.get('/', async (req, res, next)=>{
@@ -35,6 +36,8 @@ router.post("/add", async (req,res)=>{
 
         await category.save();
 
+        AuditLogs.info(req.user?.email, "Categories", "Add", {_id: body._id, category});
+
         res.json(Response.successResponse({success: true}));
 
     } catch (err) {
@@ -55,6 +58,8 @@ router.post("/update", async (req,res)=>{
 
         await Categories.updateOne({ _id: body._id}, updates);
 
+        AuditLogs.info(req.user?.email, "Categories", "Update", {_id: body._id, ...updates});
+
         res.json(Response.successResponse({success: true}));
     } catch (err) {
         let errorResponse = Response.errorResponse(err);
@@ -68,6 +73,8 @@ router.post("/delete", async (req,res)=>{
         if(!body._id) throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error!", "_id field must be filled");
 
         await Categories.deleteOne({_id: body._id});
+
+        AuditLogs.info(req.user?.email, "Categories", "Delete", {_id: body._id});
 
         res.json(Response.successResponse({success: true}));
 
